559b72bb420ba0cb10d4e7df4f5aea6a99575a69
MrFish0228 <nikich331@gmail.com>
2023-02-12 09:29:12 +0700

    First task

---
+++ Lesson1/First task/Program.cs
@@ -0,0 +1,15 @@
+﻿int num1, num2;
+
+Console.Write($"Введите первое число: ");
+int.TryParse(Console.ReadLine()!, out num1);
+Console.Write($"Введите второе число: ");
+int.TryParse(Console.ReadLine()!, out num2);
+
+if (num1 > num2)
+{
+    Console.Write($"Максимальное число: " + num1);
+}
+else
+{
+    Console.Write($"Максимальное число: " + num2);
+}
